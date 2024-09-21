eb10704a2828dbfe2f1997069c497e6e95f2dbac
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-09-22 02:39:39 +0900

    Add new cop: Style/RbsInline/InvalidTypes

--- lib/rubocop/cop/rbs_inline_cops.rb
+++ lib/rubocop/cop/rbs_inline_cops.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'style/rbs_inline/invalid_comment'
+require_relative 'style/rbs_inline/invalid_types'
 require_relative 'style/rbs_inline/keyword_separator'
 require_relative 'style/rbs_inline/parameters_separator'
 require_relative 'style/rbs_inline/unused_annotations'
