d2993ad28e30263abfa2ff4038085a0ad14c1b8e
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-10-06 02:46:06 +0900

    UnmatchedAnnotations: Support singleton method definitions

--- spec/rubocop/cop/style/rbs_inline/unmatched_annotations_spec.rb
+++ spec/rubocop/cop/style/rbs_inline/unmatched_annotations_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe RuboCop::Cop::Style::RbsInline::UnmatchedAnnotations, :config do
   let(:config) { RuboCop::Config.new }
 
-  context 'when an annotation comment found above the method definition' do
+  context 'when an annotation comment found above the instance method definition' do
     context 'when the comment annotates to unknown argument' do
       it 'registers an offense' do
         expect_offense(<<~RUBY)
@@ -36,6 +36,44 @@ RSpec.describe RuboCop::Cop::Style::RbsInline::UnmatchedAnnotations, :config do
           # @rbs &: String
           # @rbs return: String
           def method(arg1, arg2 = nil, *args, kwarg1:, kwarg2: nil, **kwargs, &block); end
+        RUBY
+      end
+    end
+  end
+
+  context 'when an annotation comment found above the singleton method definition' do
+    context 'when the comment annotates to unknown argument' do
+      it 'registers an offense' do
+        expect_offense(<<~RUBY)
+          # Comments including multibyte characters: あいうえお
+
+          # @rbs unknown: String
+                 ^^^^^^^ Style/RbsInline/UnmatchedAnnotations: target parameter not found.
+          # @rbs &unknown: String
+                 ^^^^^^^^ Style/RbsInline/UnmatchedAnnotations: target parameter not found.
+          def self.method(arg1, arg2 = nil, *args, kwarg1:, kwarg2: nil, **kwargs, &block); end
+        RUBY
+      end
+    end
+
+    context 'when the comment annotates to known arguments' do
+      it 'does not register an offense' do
+        expect_no_offenses(<<~RUBY)
+          # Comments including multibyte characters: あいうえお
+          # @rbs @arg1: String
+
+          # @rbs arg1: String
+          # @rbs arg2: String
+          # @rbs *args: String
+          # @rbs *: String
+          # @rbs kwarg1: String
+          # @rbs kwarg2: String
+          # @rbs **kwargs: String
+          # @rbs **: String
+          # @rbs &block: String
+          # @rbs &: String
+          # @rbs return: String
+          def self.method(arg1, arg2 = nil, *args, kwarg1:, kwarg2: nil, **kwargs, &block); end
         RUBY
       end
     end
